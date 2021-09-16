ba1b9ffffc6beadb1493bad2b4ddb754f402f8ba
Aleksbielarus <aleksbielarus@gmail.com>
2021-09-16 15:11:19 +0300

    Fix list_gen

--- Module_1.py
+++ Module_1.py
@@ -9,10 +9,7 @@ import random
         cnt - it's a count of numbers should be generated.
 '''
 def list_gen(first, last, cnt):
-    numbers = list()
-    for i in range(0, cnt):
-        number = random.randrange(first, last)
-        numbers.append(number)
+    numbers = [random.randrange(first, last) for number in range(0, cnt)]
     return numbers
 
 
