b91748f75857852aeb12a9a4133e0fe46f34f15b
Aleksbielarus <aleksbielarus@gmail.com>
2021-10-11 17:05:55 +0300

    Module 5. add import from file menu for task 6.

--- Module_5.py
+++ Module_5.py
@@ -66,60 +66,78 @@ class Weather(Note):
 def console():
     type = str()
     cnt = 0
-    welcome_text = 'please, choose the note type:\n' \
+    welcome_message = 'please choose input type:\n' \
+                       ' input "1" if manual\n' \
+                       ' input "2" if using import from file\n' \
+                       ' input "0" if you want to stop'
+    manual_input = 'please, choose the note type:\n' \
                    ' input "1" if news\n' \
                    ' input "2" if private ad\n' \
                    ' input "3" if weather\n' \
                    ' input "0" if you want to stop'
+    import_input = 'import file menu'
     while type != 0:
-        # need to add try/except.
-        print('Hi, ' + welcome_text) if cnt == 0 else print(welcome_text.capitalize())
+        print('Hi, ' + welcome_message) if cnt == 0 else print(welcome_message.capitalize())
         type = input('> ')
         # ERROR MESSAGE DATA TYPE
         try:
             type = int(type)
             # NOT VALID TYPE
-            if int(type) not in [1, 2, 3, 0]:
+            if int(type) not in [1, 2, 0]:
                 print(f'"{type}" is not valid value. Please, try again')
-            # NEWS NOTE
             elif int(type) == 1:
-                note_text = input("Please input message text:\n> ").capitalize()
-                note_city = input("Please input message city:\n> ").capitalize()
-                new_note = News('News', note_text, note_city)
-                insert_row = f"{new_note.print_type()} {(30 - len(new_note.print_type())) * '-'}\n" \
-                             f"{new_note.print_note_text()}\n" \
-                             f"{new_note.print_city()}, {new_note.print_datetime()}\n" \
-                             f"{31 * '-'}\n"
-                add_new_note(insert_row)
-                print(f'Message with type {type} was created.')
-                type = int(input('Do you whish continue to add news?\n print "1" if yes\n print "0" if no.\n> '))
-            # PRIVATE AD
-            elif type == 2:
-                note_text = input("Please input message text:\n> ").capitalize()
-                date = input("Please input message date(example '31/10/2021':\n> ")
-                new_note = PrivateAd('Private Add', note_text, date)
-                insert_row = f"{new_note.print_type()} {(30 - len(new_note.print_type())) * '-'}\n" \
-                             f"{new_note.print_note_text()}\n" \
-                             f"Actual until: {new_note.print_date()}, {new_note.print_days_left()} days left\n" \
-                             f"{31 * '-'}\n"
-                add_new_note(insert_row)
-                print(f'Message with type {type} was created.')
-                type = int(input('Do you whish continue to add news?\n print "1" if yes\n print "0" if no.\n> '))
-            # MY UNIQUE NOTE (WHETHER)
-            elif type == 3:
-                note_text = input("Please input message text:\n> ").capitalize()
-                note_city = input("Please input message city:\n> ").capitalize()
-                note_degrees = input("Please input degrees:\n> ").capitalize()
-                date = input("Please input message date(example '31/10/2021'):\n> ")
-                new_note = Weather('Weather', note_text, note_city, date, note_degrees)
-                insert_row = f"{new_note.print_type()} {(30 - len(new_note.print_type())) * '-'}\n" \
-                             f"{new_note.print_note_text()}\n" \
-                             f"{new_note.print_city()}, {new_note.print_degrees()} degrees {new_note.print_date()}\n" \
-                             f"{31 * '-'}\n"
-                add_new_note(insert_row)
-                print(f'Message with type {type} was created.')
-                type = int(input('Do you whish continue to add news?\n print "1" if yes\n print "0" if no.\n> '))
-        # TYPE ERROR
+                print(manual_input.capitalize())
+                type = input('> ')
+                # ERROR MESSAGE DATA TYPE
+                try:
+                    type = int(type)
+                    # NOT VALID TYPE
+                    if int(type) not in [1, 2, 3, 0]:
+                        print(f'"{type}" is not valid value. Please, try again')
+                    # NEWS NOTE
+                    elif int(type) == 1:
+                        note_text = input("Please input message text:\n> ").capitalize()
+                        note_city = input("Please input message city:\n> ").capitalize()
+                        new_note = News('News', note_text, note_city)
+                        insert_row = f"{new_note.print_type()} {(30 - len(new_note.print_type())) * '-'}\n" \
+                                     f"{new_note.print_note_text()}\n" \
+                                     f"{new_note.print_city()}, {new_note.print_datetime()}\n" \
+                                     f"{31 * '-'}\n"
+                        add_new_note(insert_row)
+                        print(f'Message with type {type} was created.')
+                        type = int(input('Do you wish continue to add news?\n print "1" if yes\n print "0" if no.\n> '))
+                    # PRIVATE AD
+                    elif type == 2:
+                        note_text = input("Please input message text:\n> ").capitalize()
+                        date = input("Please input message date(example '31/10/2021':\n> ")
+                        new_note = PrivateAd('Private Add', note_text, date)
+                        insert_row = f"{new_note.print_type()} {(30 - len(new_note.print_type())) * '-'}\n" \
+                                     f"{new_note.print_note_text()}\n" \
+                                     f"Actual until: {new_note.print_date()}, {new_note.print_days_left()} days left\n" \
+                                     f"{31 * '-'}\n"
+                        add_new_note(insert_row)
+                        print(f'Message with type {type} was created.')
+                        type = int(input('Do you wish continue to add news?\n print "1" if yes\n print "0" if no.\n> '))
+                    # MY UNIQUE NOTE (WHETHER)
+                    elif type == 3:
+                        note_text = input("Please input message text:\n> ").capitalize()
+                        note_city = input("Please input message city:\n> ").capitalize()
+                        note_degrees = input("Please input degrees:\n> ").capitalize()
+                        date = input("Please input message date(example '31/10/2021'):\n> ")
+                        new_note = Weather('Weather', note_text, note_city, date, note_degrees)
+                        insert_row = f"{new_note.print_type()} {(30 - len(new_note.print_type())) * '-'}\n" \
+                                     f"{new_note.print_note_text()}\n" \
+                                     f"{new_note.print_city()}, {new_note.print_degrees()} degrees {new_note.print_date()}\n" \
+                                     f"{31 * '-'}\n"
+                        add_new_note(insert_row)
+                        print(f'Message with type {type} was created.')
+                        type = int(input('Do you wish continue to add news?\n print "1" if yes\n print "0" if no.\n> '))
+                # TYPE ERROR
+                except:
+                    print('Not valid data type, please try again.')
+            elif int(type) == 2:
+                print(import_input)
+                type = input('> ')
         except:
             print('Not valid data type, please try again.')
         # EXIT
