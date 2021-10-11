48f484305aa2bd4e9aa4c1b5cbd45821cdf28843
Aleksbielarus <aleksbielarus@gmail.com>
2021-10-11 14:04:53 +0300

    Module 4. up to code

--- Module_4.py
+++ Module_4.py
@@ -100,17 +100,17 @@ def split_dict_from_list(dict_list):
 # create a variable with the text from hometask.
 text = '''homEwork:
   tHis iz your homeWork, copy these Text to variable.
-
-
-
+  
+  
+  
   You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
-
-
-
+  
+  
+  
   it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
-
-
-
+  
+  
+  
   last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87.
 '''
 
