dbec869832aa8e39b4dbb6b03a295c65420f346a
Aleksbielarus <aleksbielarus@gmail.com>
2021-10-13 13:13:56 +0300

    Module 6. structure v2.

--- Module_6/Console.py
+++ Module_6/Console.py
@@ -1,37 +1,33 @@
 from datetime import date
 from datetime import datetime
-
-
-# add new note to text file.
-def add_new_note(new_note):
-    with open("file.txt", "a") as text_file:
-        text_file.write(new_note + '\n')
+from package import console_menu
+import os
 
 
 class Note:
     datetime = datetime.now().strftime('%d/%m/%Y %H.%M')
-    date = date.today().strftime('%d/%m/%Y')
+    note_date = date.today().strftime('%d/%m/%Y')
 
-    def __init__(self, type, note_text):
-        self.type = type
+    def __init__(self, flag, note_text):
+        self.flag = flag
         self.note_text = note_text
 
     def print_type(self):
-        return self.type
+        return self.flag
 
     def print_note_text(self):
         return self.note_text
 
     def print_date(self):
-        return self.date
+        return self.note_date
 
     def print_datetime(self):
         return self.datetime
 
 
 class News(Note):
-    def __init__(self, type, note_text, city):
-        Note.__init__(self, type=type, note_text=note_text)
+    def __init__(self, flag, note_text, city):
+        Note.__init__(self, flag=flag, note_text=note_text)
         self.city = city
 
     def print_city(self):
@@ -39,21 +35,21 @@ class News(Note):
 
 
 class PrivateAd(Note):
-    def __init__(self, type, note_text, date):
-        Note.__init__(self, type=type, note_text=note_text)
-        self.date = date
+    def __init__(self, flag, note_text, note_date):
+        Note.__init__(self, flag=flag, note_text=note_text)
+        self.note_date = note_date
 
     def print_news_text(self):
         return self.note_text
 
     def print_days_left(self):
-        return (datetime.strptime(self.date, '%d/%m/%Y') - datetime.now()).days
+        return (datetime.strptime(self.note_date, '%d/%m/%Y') - datetime.now()).days
 
 
 class Weather(Note):
-    def __init__(self, type, note_text, city, date, degrees):
-        Note.__init__(self, type=type, note_text=note_text)
-        self.date = date
+    def __init__(self, flag, note_text, city, note_date, degrees):
+        Note.__init__(self, flag=flag, note_text=note_text)
+        self.note_date = note_date
         self.degrees = degrees
         self.city = city
 
@@ -63,110 +59,139 @@ class Weather(Note):
     def print_city(self):
         return self.city
 
+
+# add new note to text file.
+def add_new_note(new_note):
+    with open("file.txt", "a") as text_file:
+        text_file.write(new_note + '\n')
+
+
+def news(flag):
+    note_text = console_menu.note_text()
+    note_city = console_menu.note_city()
+    new_note = News('News', note_text, note_city)
+    insert_row = f"{new_note.print_type()} {(30 - len(new_note.print_type())) * '-'}\n" \
+                 f"{new_note.print_note_text()}\n" \
+                 f"{new_note.print_city()}, {new_note.print_datetime()}\n" \
+                 f"{31 * '-'}\n"
+    add_new_note(insert_row)
+    print(console_menu.creation_confirm_message(flag))
+
+
+def private_add(flag):
+    note_text = console_menu.note_text()
+    note_date = console_menu.note_date()
+    new_note = PrivateAd('Private Add', note_text, note_date)
+    insert_row = f"{new_note.print_type()} {(30 - len(new_note.print_type())) * '-'}\n" \
+                 f"{new_note.print_note_text()}\n" \
+                 f"Actual until: {new_note.print_date()}, {new_note.print_days_left()} days left\n" \
+                 f"{31 * '-'}\n"
+    add_new_note(insert_row)
+    print(console_menu.creation_confirm_message(flag))
+
+
+def weather(flag):
+    note_text = console_menu.note_text()
+    note_city = console_menu.note_city()
+    note_degrees = console_menu.note_degrees()
+    note_date = console_menu.note_date()
+    new_note = Weather('Weather', note_text, note_city, note_date, note_degrees)
+    insert_row = f"{new_note.print_type()} {(30 - len(new_note.print_type())) * '-'}\n" \
+                 f"{new_note.print_note_text()}\n" \
+                 f"{new_note.print_city()}, {new_note.print_degrees()} degrees {new_note.print_date()}\n" \
+                 f"{31 * '-'}\n"
+    add_new_note(insert_row)
+    print(console_menu.creation_confirm_message(flag))
+
+
+def import_load(flag):
+    try:
+        flag = int(flag)
+        # NOT VALID TYPE
+        if int(flag) not in [1, 2, 3, 4, 0]:
+            print(f'"{flag}" is not valid value. Please, try again')
+        # IMPORT FROM CUSTOM FILE FORMAT
+        elif flag == 1:
+            path_to_file = str(input("Please, specify the file path\n> "))
+            # specify default path to file(current directory)
+            # !!!! need to add strong specification to current dir.
+            if path_to_file == '':
+                path_to_file = os.getcwd()
+            file_text = list(open(path_to_file, 'r'))
+            print(file_text)
+        # IMPORT FROM CSV FILE FORMAT
+        elif flag == 2:
+            print(console_menu.error_unavailable_now())
+        # IMPORT FROM JSON FILE FORMAT
+        elif flag == 3:
+            print(console_menu.error_unavailable_now())
+        # IMPORT FROM XML FILE FORMAT
+        elif flag == 4:
+            print(console_menu.error_unavailable_now())
+    except ValueError:
+        print(console_menu.error_import_type())
+
+
+def input_note(input_type, flag, note_type='news'):  # default param is news
+    if input_type == 'm':
+        if note_type.lower() == 'news':
+            news(flag)
+        elif note_type.lower() == 'private add':
+            private_add(flag)
+        elif note_type.lower() == 'weather':
+            weather(flag)
+    elif input_type == 'i':
+        import_load(flag)
+    else:
+        print('error')
+
+
 def console():
-    type = str()
+    flag = str()
     cnt = 0
-    welcome_message = 'please choose input type:\n' \
-                       ' input "1" if manual\n' \
-                       ' input "2" if using import from file\n' \
-                       ' input "0" if you want to stop'
-    manual_input = 'please, choose the note type:\n' \
-                   ' input "1" if news\n' \
-                   ' input "2" if private ad\n' \
-                   ' input "3" if weather\n' \
-                   ' input "0" if you want to stop'
-    import_input = 'please, choose the import  file format:\n' \
-                   ' input "1" from custom format\n' \
-                   ' input "2" from csv format\n' \
-                   ' input "3" from json format\n' \
-                   ' input "4" from xml format\n' \
-                   ' input "0" if you want to stop'
-    while type != 0:
+    welcome_message = console_menu.welcome_message()
+    manual_input = console_menu.manual_input()
+    import_input = console_menu.import_input()
+    while flag != 0:
         print('Hi, ' + welcome_message) if cnt == 0 else print(welcome_message.capitalize())
-        type = input('> ')
+        flag = input('> ')
         # ERROR MESSAGE DATA TYPE
         try:
-            type = int(type)
+            flag = int(flag)
             # NOT VALID TYPE
-            if int(type) not in [1, 2, 0]:
-                print(f'"{type}" is not valid value. Please, try again')
-            elif int(type) == 1:
+            if int(flag) not in [1, 2, 0]:
+                print(console_menu.error_not_valid_flag(flag))
+            elif int(flag) == 1:
                 print(manual_input.capitalize())
-                type = input('> ')
+                flag = input('> ')
                 # ERROR MESSAGE DATA TYPE
                 try:
-                    type = int(type)
+                    flag = int(flag)
                     # NOT VALID TYPE
-                    if int(type) not in [1, 2, 3, 0]:
-                        print(f'"{type}" is not valid value. Please, try again')
+                    if int(flag) not in [1, 2, 3, 0]:
+                        print(console_menu.error_not_valid_flag(flag))
                     # NEWS NOTE
-                    elif int(type) == 1:
-                        note_text = input("Please input message text:\n> ").capitalize()
-                        note_city = input("Please input message city:\n> ").capitalize()
-                        new_note = News('News', note_text, note_city)
-                        insert_row = f"{new_note.print_type()} {(30 - len(new_note.print_type())) * '-'}\n" \
-                                     f"{new_note.print_note_text()}\n" \
-                                     f"{new_note.print_city()}, {new_note.print_datetime()}\n" \
-                                     f"{31 * '-'}\n"
-                        add_new_note(insert_row)
-                        print(f'Message with type {type} was created.')
-                        type = int(input('Do you wish continue to add news?\n print "1" if yes\n print "0" if no.\n> '))
+                    elif int(flag) == 1:
+                        input_note('m', 1, 'news')
                     # PRIVATE AD
-                    elif type == 2:
-                        note_text = input("Please input message text:\n> ").capitalize()
-                        date = input("Please input message date(example '31/10/2021':\n> ")
-                        new_note = PrivateAd('Private Add', note_text, date)
-                        insert_row = f"{new_note.print_type()} {(30 - len(new_note.print_type())) * '-'}\n" \
-                                     f"{new_note.print_note_text()}\n" \
-                                     f"Actual until: {new_note.print_date()}, {new_note.print_days_left()} days left\n" \
-                                     f"{31 * '-'}\n"
-                        add_new_note(insert_row)
-                        print(f'Message with type {type} was created.')
-                        type = int(input('Do you wish continue to add news?\n print "1" if yes\n print "0" if no.\n> '))
-                    # MY UNIQUE NOTE (WHETHER)
-                    elif type == 3:
-                        note_text = input("Please input message text:\n> ").capitalize()
-                        note_city = input("Please input message city:\n> ").capitalize()
-                        note_degrees = input("Please input degrees:\n> ").capitalize()
-                        date = input("Please input message date(example '31/10/2021'):\n> ")
-                        new_note = Weather('Weather', note_text, note_city, date, note_degrees)
-                        insert_row = f"{new_note.print_type()} {(30 - len(new_note.print_type())) * '-'}\n" \
-                                     f"{new_note.print_note_text()}\n" \
-                                     f"{new_note.print_city()}, {new_note.print_degrees()} degrees {new_note.print_date()}\n" \
-                                     f"{31 * '-'}\n"
-                        add_new_note(insert_row)
-                        print(f'Message with type {type} was created.')
-                        type = int(input('Do you wish continue to add news?\n print "1" if yes\n print "0" if no.\n> '))
+                    elif flag == 2:
+                        input_note('m', 2, 'private add')
+                    # MY UNIQUE NOTE (WEATHER)
+                    elif flag == 3:
+                        input_note('m', 3, 'weather')
                 # TYPE ERROR
-                except:
-                    print('Not valid data type, please try again.')
-            elif int(type) == 2:
+                except ValueError:
+                    print(console_menu.error_date_type())
+                flag = console_menu.continue_massage()
+            elif int(flag) == 2:
                 print(import_input)
-                type = input('> ')
-                try:
-                    type = int(type)
-                    # NOT VALID TYPE
-                    if int(type) not in [1, 2, 3, 4, 0]:
-                        print(f'"{type}" is not valid value. Please, try again')
-                    # IMPORT FROM CUSTOM FILE FORMAT
-                    elif type == 1:
-                        path_to_file = input("Please, specify the file path\n> ")
-                    # IMPORT FROM CSV FILE FORMAT
-                    elif type == 2:
-                        print("Will be available soon. Please choose another file format")
-                    # IMPORT FROM JSON FILE FORMAT
-                    elif type == 3:
-                        print("Will be available soon. Please choose another file format")
-                    # IMPORT FROM XML FILE FORMAT
-                    elif type == 4:
-                        print("Will be available soon. Please choose another file format")
-                except:
-                    pass
-        except:
-            print('Not valid data type, please try again.')
+                flag = input('> ')
+                input_note('i', flag)
+        except ValueError:
+            print(console_menu.error_date_type())
         # EXIT
-        if type == 0:
-            print("Have a good day!")
+        if flag == 0:
+            print(console_menu.goodbye_message())
         cnt += 1
 
 
---
+++ Module_6/package/console_menu.py
@@ -0,0 +1,69 @@
+#  MANUAL INPUT FUNCTIONS
+def note_text():
+    return input("Please input message text:\n> ").capitalize()
+
+
+def note_city():
+    return input("Please input message city:\n> ").capitalize()
+
+
+def note_date():
+    return input("Please input message date(example '31/10/2021':\n> ")
+
+
+def note_degrees():
+    return input("Please input degrees:\n> ").capitalize()
+
+
+# OTHER MESSAGES
+def welcome_message():
+    return 'please choose input type:\n' \
+                       ' input "1" if manual\n' \
+                       ' input "2" if using import from file\n' \
+                       ' input "0" if you want to stop'
+
+
+def manual_input():
+    return 'please, choose the note type:\n' \
+                   ' input "1" if news\n' \
+                   ' input "2" if private ad\n' \
+                   ' input "3" if weather\n' \
+                   ' input "0" if you want to stop'
+
+
+def import_input():
+    return 'please, choose the import  file format:\n' \
+                   ' input "1" from custom format\n' \
+                   ' input "2" from csv format\n' \
+                   ' input "3" from json format\n' \
+                   ' input "4" from xml format\n' \
+                   ' input "0" if you want to stop'
+
+
+def creation_confirm_message(flag):
+    return f'Message with type {flag} was created.'
+
+
+def continue_massage():
+    return int(input('Do you wish continue to add news?\n print "1" if yes\n print "0" if no.\n> '))
+
+
+def goodbye_message():
+    return "Have a good day!"
+
+
+# ERROR MESSAGES
+def error_unavailable_now():
+    return "Will be available soon. Please choose another file format"
+
+
+def error_not_valid_flag(flag):
+    return f'"{flag}" is not valid value. Please, try again'
+
+
+def error_import_type():
+    return 'Not valid import type.'
+
+
+def error_date_type():
+    return 'Not valid data type, please try again.'
